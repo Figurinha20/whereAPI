3aa54ec15a7dddd102b0dcddd4c6a9cb9f16948e
José Pedro <9180270@esmad.ipp.pt>
2020-04-16 17:46:58 +0100

    Class Restaurante

---
+++ classes/Restaurante.js
@@ -0,0 +1,15 @@
+class Restaurante{
+    constructor(id,nome,password,foto_perfil,informacao,morada,aprovacao,cod_postal,disponibilidade,email){
+        this.id = id;
+        this.nome = nome;
+        this.password=password;
+        this.foto_perfil =foto_perfil;
+        this.informacao=informacao;
+        this.morada=morada;
+        this.aprovacao=aprovacao;
+        this.cod_postal=cod_postal;
+        this.disponibilidade=disponibilidade;
+        this.email=email;
+    }
+}
+module.exports =Restaurante;
