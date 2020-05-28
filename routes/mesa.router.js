117231e41d3b50b8e612a9e059470d7d49b2276f
Figurinha20 <figurinhapt@gmail.com>
2020-05-28 21:15:47 +0100

    Espaço a mais numa route

--- routes/mesa.router.js
+++ routes/mesa.router.js
@@ -4,7 +4,7 @@ var router=Router();
 
 router.get("/restaurantes/:id_restaurante/mesas", MesaControllers.get);
 router.post("/restaurantes/:id_restaurante/mesas", MesaControllers.post);
-router.delete("/restaurantes/mesas/:id_mesa ", MesaControllers.delete);
+router.delete("/restaurantes/mesas/:id_mesa", MesaControllers.delete);
 
 module.exports=router;
 
