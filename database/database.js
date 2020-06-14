e2b1b0b86b669c60cebfae3778cb71ab2290884e
José Pedro <9180270@esmad.ipp.pt>
2020-06-14 18:02:33 +0100

    database solved

--- database/database.js
+++ database/database.js
@@ -4,7 +4,11 @@ const util = require("util");
 
 const Database=mysql.createConnection(dbConfig);    //criar a ligação a base de dados usando o config importado e guardar na variavel Database
 Database.connect((err)=>{err ? console.log(err) : console.log("Connection established to database");}); //ligar a base de dados e fazer log se a conection foi sucedida
-
+Database.on("error",(error)=>{
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') {
+        Database.connect((err)=>{err ? console.log(err) : console.log("Connection established to database");});
+    }
+});
 Database.query=util.promisify(Database.query);  //transformar callback para promise para faclitar o uso
 
 module.exports = Database;  //Exportar a variavel Database que tem a connection criada
