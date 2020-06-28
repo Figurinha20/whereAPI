39b206a66a3f6f63ad881fd7b6abf56be7d7e877
José Pedro <9180270@esmad.ipp.pt>
2020-06-28 17:33:40 +0100

    TRYING V5

--- database/database.js
+++ database/database.js
@@ -1,4 +1,5 @@
 let dbConfig;
+
 if(process.env.NODE_ENV === "production" || process.env.NODE_ENV === "PROD" ){
     dbConfig = {
         host: process.env.HOST,
@@ -14,7 +15,7 @@ if(process.env.NODE_ENV === "production" || process.env.NODE_ENV === "PROD" ){
  //Importar configuração da base de dados
 const mysql = require("mysql"); //bilbioteca de mysql https://www.npmjs.com/package/mysql
 const util = require("util");
-
+console.log(process.env.HOST,process.env.USER,process.env.PASSWORD,process.env.DATABASE,process.env.PORT,process.env.NODE_ENV)
 let Database=mysql.createPool(dbConfig);
  //criar a ligação a base de dados usando o config importado e guardar na variavel Database
 Database.getConnection((err)=>{err ? console.log(err) : console.log("Connection established to database");}); //ligar a base de dados e fazer log se a conection foi sucedida
