55153d34852f5d98a0649efcd0666e29528062d4
Figurinha20 <figurinhapt@gmail.com>
2020-06-28 03:01:38 +0100

    idk

--- database/database.js
+++ database/database.js
@@ -3,12 +3,12 @@ const mysql = require("mysql"); //bilbioteca de mysql https://www.npmjs.com/pack
 const util = require("util");
 
 let Database=mysql.createPool(dbConfig);    //criar a ligação a base de dados usando o config importado e guardar na variavel Database
-// Database.getConnection((err)=>{err ? console.log(err) : console.log("Connection established to database");}); //ligar a base de dados e fazer log se a conection foi sucedida
+Database.getConnection((err)=>{err ? console.log(err) : console.log("Connection established to database");}); //ligar a base de dados e fazer log se a conection foi sucedida
 Database.on("error",(error)=>{
     if(error.code === 'PROTOCOL_CONNECTION_LOST' || error.code ==='PROTOCOL_ENQUEUE_AFTER_FATAL_ERROR') {
         Database.destroy();
         Database = mysql.createPool(dbConfig);
-        // Database.connect((err)=>{err ? console.log(err) : console.log("Connection established to database");});
+        Database.connect((err)=>{err ? console.log(err) : console.log("Connection established to database");});
     }
 });
 Database.query=util.promisify(Database.query);  //transformar callback para promise para faclitar o uso
