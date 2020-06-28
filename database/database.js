6ab48e573ff3a18631f5519bd71a93bb54460e44
José Pedro <9180270@esmad.ipp.pt>
2020-06-28 17:14:06 +0100

    trying v4

--- database/database.js
+++ database/database.js
@@ -1,15 +1,22 @@
-console.log(process.env.TEST) 
-const dbConfig = require("./db-config.json");   //Importar configuração da base de dados
+let dbConfig;
+if(process.env.NODE_ENV === "production" || process.env.NODE_ENV === "PROD" ){
+    dbConfig = {
+        host: process.env.HOST,
+        user: process.env.USER,
+        password: process.env.PASSWORD,
+        database: process.env.DATABASE,
+        port: process.env.PORT,
+        multipleStatements: true      
+    }
+}else{
+    dbConfig = require("./db-config.json");
+}
+ //Importar configuração da base de dados
 const mysql = require("mysql"); //bilbioteca de mysql https://www.npmjs.com/package/mysql
 const util = require("util");
 
 let Database=mysql.createPool(dbConfig);
-process.env.PORT = dbConfig.port  
-process.env.HOST = dbConfig.host 
-process.env.USER = dbConfig.user
-process.env.PASSWORD = dbConfig.password
-process.env.DATABASE = dbConfig.database
-process.env.MULTIPLESTATEMENTS = dbConfig.multipleStatements    //criar a ligação a base de dados usando o config importado e guardar na variavel Database
+ //criar a ligação a base de dados usando o config importado e guardar na variavel Database
 Database.getConnection((err)=>{err ? console.log(err) : console.log("Connection established to database");}); //ligar a base de dados e fazer log se a conection foi sucedida
 Database.on("error",(error)=>{
     if(error.code === 'PROTOCOL_CONNECTION_LOST' || error.code ==='PROTOCOL_ENQUEUE_AFTER_FATAL_ERROR') {
